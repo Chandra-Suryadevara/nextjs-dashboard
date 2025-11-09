554711f56c3a1b3ad7eef20f4aa5330cc8fc23f6
Chandra <chandrashekar.suryad@uleth.ca>
2025-11-09 01:00:46 -0700

    updated

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,7 +16,7 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
+    console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
@@ -38,10 +38,11 @@ export async function fetchLatestInvoices() {
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
-
+      // await new Promise((resolve) => setTimeout(resolve, 3000));
     const latestInvoices = data.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
+      
     }));
     return latestInvoices;
   } catch (error) {
